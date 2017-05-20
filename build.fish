5abb111029a88d08ffb37fea0fd6c65a26d64e35
reinvdwoerd <reinvanderwoerd@me.com>
2017-05-20 17:12:53 +0200

    Build

--- build.fish
+++ build.fish
@@ -2,6 +2,9 @@ pandoc manuscript/*.md -o book.pdf \
   --latex-engine=xelatex \
   --table-of-contents \
   --smart \
-  --variable mainfont="Equity Text A" \
-  --variable sansfont="Concourse T3" \
-  --variable monofont="Fira Code" \
+  -V mainfont="Concourse T3" \
+  -V sansfont="Concourse T3" \
+  -V monofont="Fira Mono" \
+  -V documentclass=book \
+  --highlight-style tango
+  
